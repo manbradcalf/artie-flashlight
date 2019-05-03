2a0331d05089938128dac79178949a7510de1803
manbradcalf <ben.medcalf@gmail.com>
2019-05-03 19:04:11 -0400

    updating results formatting

--- example/example.js
+++ example/example.js
@@ -46,96 +46,110 @@
 
   function buildQuery($form) {
     // this just gets data out of the form
-    var index = $form.find('[name=index]').val();
-    var type = $form.find('[name="type"]:checked').val();
+    var index = $form.find('[name="index"]:checked').val();
     var term = $form.find('[name="term"]').val();
-    var matchWholePhrase = $form.find('[name="exact"]').is(':checked');
-    var size = parseInt($form.find('[name="size"]').val());
-    var from = parseInt($form.find('[name="from"]').val());
+    // var matchWholePhrase = $form.find('[name="exact"]').is(':checked');
+    // var size = parseInt($form.find('[name="size"]').val());
+    // var from = parseInt($form.find('[name="from"]').val());
 
     // skeleton of the JSON object we will write to DB
     var query = {
       index: index,
-      type: type
+      term: term
     };
 
-    // size and from are used for pagination
-    if( !isNaN(size) ) { query.size = size; }
-    if( !isNaN(from) ) { query.from = from; }
+    // // size and from are used for pagination
+    // if( !isNaN(size) ) { query.size = size; }
+    // if( !isNaN(from) ) { query.from = from; }
 
-    buildQueryBody(query, term, matchWholePhrase);
+    // buildQueryBody(query, term, matchWholePhrase);
 
     return query;
   }
 
-  function buildQueryBody(query, term, matchWholePhrase) {
-    if( matchWholePhrase ) {
-      var body = query.body = {};
-      body.query = {
-        // match_phrase matches the phrase exactly instead of breaking it
-        // into individual words
-        "match_phrase": {
-          // this is the field name, _all is a meta indicating any field
-          "_all": term
-        }
-        /**
-         * Match breaks up individual words and matches any
-         * This is the equivalent of the `q` string below
-        "match": {
-          "_all": term
-        }
-        */
-      }
-    }
-    else {
-      query.q = term;
-    }
-  }
+  // function buildQueryBody(query, term, matchWholePhrase) {
+  //   if( matchWholePhrase ) {
+  //     var body = query.body = {};
+  //     body.query = {
+  //       // match_phrase matches the phrase exactly instead of breaking it
+  //       // into individual words
+  //       "match_phrase": {
+  //         // this is the field name, _all is a meta indicating any field
+  //         "_all": term
+  //       }
+  //       /**
+  //        * Match breaks up individual words and matches any
+  //        * This is the equivalent of the `q` string below
+  //       "match": {
+  //         "_all": term
+  //       }
+  //       */
+  //     }
+  //   }
+  //   else {
+  //     query.q = term;
+  //   }
+  // }
 
   // !!! TODO:  Refactor doSearch and showResults so they query ES directly !!!
 
   // conduct a search by writing it to the search/request path
   function doSearch(query) {
-    var ref = database.ref().child(PATH);
-    var key = ref.child('request').push(query).key;
+    // var ref = database.ref().child(PATH);
+    // var key = ref.child('request').push(query).key;
 
-    console.log('search', key, query);
-    $('#query').text(JSON.stringify(query, null, 2));
-    ref.child('response/'+key).on('value', showResults);
+    // console.log('search', key, query);
+    // $('#query').text(JSON.stringify(query, null, 2));
+    // ref.child('response/'+key).on('value', showResults);
+    const URL = `https://dogwood-9512546.us-east-1.bonsaisearch.net/${query.index}/_search?q=${query.term}`
+
+    $.get(URL, function(data, status){
+
+        // Initialize results string
+        var results = '';
+        Object.keys(data.hits.hits).forEach(key => {
+          // Add each item in hits array to results
+          var result = JSON.stringify(data.hits.hits[key]._source)
+          results += result + "\n"
+          console.log("result is now " + results)
+        })
+        showResults(results)
+    })
   }
 
   // when results are written to the database, read them and display
-  function showResults(snap) {
-    if( !snap.exists() ) { return; } // wait until we get data
-    var dat = snap.val().hits;
+  function showResults(results) {
+    // if( !snap.exists() ) { return; } // wait until we get data
+    // var dat = snap.val().hits;
 
-    // when a value arrives from the database, stop listening
-    // and remove the temporary data from the database
-    snap.ref.off('value', showResults);
-    snap.ref.remove();
+    // // when a value arrives from the database, stop listening
+    // // and remove the temporary data from the database
+    // snap.ref.off('value', showResults);
+    // snap.ref.remove();
 
     // the rest of this just displays data in our demo and probably
     // isn't very interesting
-    var totalText = dat.total;
-    if( dat.hits && dat.hits.length !== dat.total ) {
-      totalText = dat.hits.length + ' of ' + dat.total;
-    }
-    $('#total').text('(' + totalText + ')');
+    // var totalText = dat.total;
+    // if( dat.hits && dat.hits.length !== dat.total ) {
+    //   totalText = dat.hits.length + ' of ' + dat.total;
+    // }
+    // $('#total').text('(' + totalText + ')');
 
-    var $pair = $('#results')
-      .text(JSON.stringify(dat, null, 2))
-      .removeClass('error zero');
-    if( dat.error ) {
-      $pair.addClass('error');
-    }
-    else if( dat.total < 1 ) {
-      $pair.addClass('zero');
-    }
+    // var $pair = $('#results')
+    //   .text(JSON.stringify(dat, null, 2))
+    //   .removeClass('error zero');
+    // if( dat.error ) {
+    //   $pair.addClass('error');
+    // }
+    // else if( dat.total < 1 ) {
+    //   $pair.addClass('zero');
+    // }
+$('#results').text(results, null, 2);
   }
 
   // display raw data for reference, this is just for the demo
   // and probably not very interesting
-  database.ref().on('value', setRawData);
+ 
   function setRawData(snap) {
     $('#raw').text(JSON.stringify(snap.val(), null, 2));
   }
